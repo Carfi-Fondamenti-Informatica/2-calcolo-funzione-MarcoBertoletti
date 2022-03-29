f52c1ef04eaba38edf54d9bf24bbaa6185e51cb6
Marco Bertoletti <100340284+MarcoBertoletti@users.noreply.github.com>
2022-03-29 19:46:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0, x=0, y=0, z=0;
+    cin >>a >>b >>x >>y;
+    if(x<0&&y>0){
+        z=a*x-b*y;
+        cout <<z;
+    }else if (x>=0&&y<=0){
+        z=a*(x*x)-b*y;
+        cout <<z;
+    }else {
+        z=a*x-b*(y*y);
+        cout <<z;
+    }
+    return 0;
 }
